4670fe5155d5ff24416350323c71f0cdc25cc2b4
Eduardo-SO <eduardo.sool07@gmail.com>
2021-03-15 15:43:39 -0300

    Add font Poppins

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -28,8 +28,16 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(--background);
     color: var(--text-body);
-    font: 1.6rem Arial, Helvetica, sans-serif;
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
